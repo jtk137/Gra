c0aa5bebc1f7f507c5df2aa3b4b52109cb8f3250
jtk137 <j.koszyk@microsoft.wsei.edu.pl>
2019-09-12 20:18:55 +0200

    Wersja 2

--- GraZaDuzoZaMalo/Monolitycznie/Program.cs
+++ GraZaDuzoZaMalo/Monolitycznie/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Monolitycznie
 {
@@ -17,14 +18,73 @@ namespace Monolitycznie
 #if DEBUG
             Console.WriteLine(wylosowana); // do usunięcia w Release
 #endif
+            Console.WriteLine("Wylosowałem liczbę z zakresu od 1 do 100. Odgadnij ją.");
+
+            Stopwatch czas = Stopwatch.StartNew();
+
             // Powtarzaj wielokrotnie, aż odgadnie
-            // 2. Człowiek proponuje  (odgaduje)
+            bool odgadniete = false;
+            int licznik = 0;
 
-            // 3. Komputer ocenia propozycję
+            do
+            {
+                licznik++;
+                // 2. Człowiek proponuje  (odgaduje)
+                Console.Write("Podaj swoją propozycję: ");
+                string napis = Console.ReadLine();
+                if (napis == "koniec" )
+                {
+                    Console.WriteLine("Szkoda, że mnie opuszczasz..");
+                    return;
+                }
 
+                int propozycja = 0;
+                try
+                {
+                    propozycja = int.Parse(napis);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nie podano liczby. \nSpróbuj jeszcze raz.");
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Przesadziłeś! Za duża liczba.");
+                    continue;
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Niezydentyfikowany wyjątek. Awaria!");
+                    Environment.Exit(1);
+                }
+
+                // 3. Komputer ocenia propozycję
+                if (propozycja < wylosowana)
+                {
+                    Console.WriteLine("Za mało.");
+                }
+                else if (propozycja > wylosowana)
+                {
+                    Console.WriteLine("Za dużo.");
+                }
+                else
+                {
+                    Console.WriteLine("Trafiłeś!");
+                    //odgadniete = true; lub while( propozycja != wylosowana ) lub
+                    break; // wychodzimy z pętli
+                }
+
+            }
+            //while (!odgadniete);
+            while (true);
             // Koniec powtarzania
 
+            czas.Stop();
+
             // 4. Wypisz statystyki gry
+            Console.WriteLine($"- Liczba ruchów: {licznik}");
+            Console.WriteLine($"- Czas gry: {czas.Elapsed}");
         }
     }
 }
